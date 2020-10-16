af89f8a7681409849ef6fc311dd352eab59b6b79
Luis Hernández <uwu.luishernandez@gmail.com>
2020-10-16 10:36:45 -0500

    change view login and dashboard and add router localstorage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,23 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-
-import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { TopComponent } from './navTop/top/top.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Importaciones para los Formularios
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+// Importaciones de Angular Material
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,7 +35,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     LoginComponent,
     RegisterComponent,
     HeaderComponent,
-    DashboardComponent
+    DashboardComponent,
+    TopComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,6 +44,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     HttpClientModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    BrowserAnimationsModule,
+    //Modulos de Formularios
+    FormsModule,
+    ReactiveFormsModule,
+    //Modulos de Material
+    MatSliderModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceService } from '../service/auth/auth-service.service';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
 
-  constructor(private serviceService : ServiceService) { }
+export class LoginComponent implements OnInit {
+  loginFormGroup: FormGroup;
+
+  constructor(private serviceService: ServiceService, private _formBuilder: FormBuilder, private _authServiceService: AuthServiceService, private _router: Router) {
+    if (_authServiceService.isAuthenticated()) {
+      _router.navigate(['dashboard'])
+    }
+  }
 
   loginG(){
     this.serviceService.getAuthGoogle();
-  } 
+  }
 
+  ngOnInit(): void {
+    this.loginFormGroup = this._formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  login(): void {
+    const data = this.loginFormGroup.value;
+    if (data.email && data.password) {
+      this._authServiceService.login(data.email, data.password).subscribe(access => {
+        localStorage.setItem('user', JSON.stringify(access));
+        this._router.navigate(['dashboard']);
+      }, error => {
+      
+      }
+      );
+    }
+  }
+  
 }
