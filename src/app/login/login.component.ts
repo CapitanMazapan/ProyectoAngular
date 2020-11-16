d72d69d538f528b2115827fe4421d816d8f79aac
Luis Hernández <uwu.luishernandez@gmail.com>
2020-11-15 18:32:03 -0600

    implement mat error for inputs from login view v1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../service.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthServiceService } from '../service/auth/auth-service.service';
 
 import { Router } from '@angular/router';
@@ -13,9 +16,13 @@ import { Router } from '@angular/router';
 
 export class LoginComponent implements OnInit {
   loginFormGroup: FormGroup;
+  usernameFormControl = new FormControl('', [Validators.required]);
+  passwordFormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
+  matcher = new MyErrorStateMatcher();
   hide = true;
+  waiting = false;
 
-  constructor(private serviceService: ServiceService, private _formBuilder: FormBuilder, private _authServiceService: AuthServiceService, private _router: Router) {
+  constructor(private _snackBar: MatSnackBar, private serviceService: ServiceService, private _formBuilder: FormBuilder, private _authServiceService: AuthServiceService, private _router: Router) {
     if (_authServiceService.isAuthenticated()) {
       _router.navigate(['dashboard'])
     }
@@ -27,22 +34,38 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.loginFormGroup = this._formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      username: this.usernameFormControl,
+      password: this.passwordFormControl,
     })
   }
 
   login(): void {
+    this.waiting = true;
     const data = this.loginFormGroup.value;
     if (data.username && data.password) {
       this._authServiceService.login(data.username, data.password).subscribe(access => {
         localStorage.setItem('user', JSON.stringify(access));
         this._router.navigate(['dashboard']);
       }, error => {
+          this.openSnackBar("The username or password is incorrect", "Close");
+          this.waiting = false;
           console.log("Datos inválidos")
       }
       );
     }
   }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 3000,
+    });
+  }
   
+}
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { ErrorStateMatcher } from '@angular/material/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { AuthServiceService } from '../service/auth/auth-service.service';
 
 import { Router } from '@angular/router';
@@ -14,22 +15,19 @@ import { Router } from '@angular/router';
 
 export class RegisterComponent implements OnInit {
   registerFormGroup: FormGroup;
-  usernameFormControl = new FormControl('', [Validators.required]);
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  password1FormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
-  password2FormControl = new FormControl('', [Validators.required, Validators.minLength(8)]);
   matcher = new MyErrorStateMatcher();
+  matcher2 = new MyErrorStateMatcher2();
   hide = true;
   hide2 = true;
 
-  constructor(private _formBuilder: FormBuilder, private _authServiceService: AuthServiceService, private _router: Router) { }
+  constructor(private _snackBar: MatSnackBar, private _formBuilder: FormBuilder, private _authServiceService: AuthServiceService, private _router: Router) { }
 
   ngOnInit(): void {
     this.registerFormGroup = this._formBuilder.group({
-      username: this.usernameFormControl,
-      email: this.emailFormControl,
-      password1: this.password1FormControl,
-      password2: this.password2FormControl,
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password1: new FormControl('', [Validators.required, Validators.minLength(8)]),
+      password2: new FormControl('', [Validators.required, Validators.minLength(8)]),
     }, { validators: this.checkPasswords });
   }
 
@@ -37,12 +35,20 @@ export class RegisterComponent implements OnInit {
     const data = this.registerFormGroup.value;
     if (data.username && data.email && data.password1 && data.password2) {
       this._authServiceService.register(data.username, data.email, data.password1, data.password2).subscribe(access => {
+        this.openSnackBar("Signed up successfully.", "Close");
         this._router.navigate(['login'])
       }, error => {
+          this.openSnackBar("There's already an account with this username or email.", "Close");
         console.log("Datos inválidos")
       }
       );
     }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this._snackBar.open(message, action, {
+      duration: 3000, 
+    });
   }
 
   checkPasswords(group: FormGroup) {
@@ -57,5 +63,14 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
     const isSubmitted = form && form.submitted;
     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+export class MyErrorStateMatcher2 implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const invalidCtrl = !!(control && control.invalid && control.parent.dirty);
+    const invalidParent = !!(control && control.parent && control.parent.invalid && control.parent.dirty);
+
+    return (invalidCtrl || invalidParent);
   }
 }
